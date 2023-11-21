6d11aab068936fa7d87c50357b9a50c40adc893d
Yann Leflour <yann@ublo.immo>
2023-11-21 17:41:16 +0100

    make button responsive

--- ui-sketcher-webview/src/ExportButton.tsx
+++ ui-sketcher-webview/src/ExportButton.tsx
@@ -48,7 +48,7 @@ export const ExportButton = () => {
           setLoading(false);
         }
       }}
-      className="fixed bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ="
+      className="fixed right-4 top-4 sm:bottom-4 sm:top-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       style={{ zIndex: 1000 }}
     >
       {loading ? (
