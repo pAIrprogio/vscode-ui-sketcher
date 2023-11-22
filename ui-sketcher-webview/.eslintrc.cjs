fdfbfc7086173ebbbb9a23031f58ab8af843e22a
Yann Leflour <yann@ublo.immo>
2023-11-22 14:26:56 +0100

    disable another crap eslint rule

--- ui-sketcher-webview/.eslintrc.cjs
+++ ui-sketcher-webview/.eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   plugins: ["react-refresh"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
